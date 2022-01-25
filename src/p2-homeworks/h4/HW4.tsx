e20dd20e240f48460d50eb2845109bc96d5f4ae2
Irina Novakovich <irinaredhood@gmail.com>
2022-01-25 21:23:19 +0300

    homework is done

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -63,7 +63,7 @@ function HW4() {
                     Life is a nightmare that prevents one from sleeping {/*// этот текст попадёт в children*/}
                 </SuperCheckbox>
 
-                {/*// onChange тоже должен работать*/}
+                {/*//onChange тоже должен работать*/}
                 <SuperCheckbox checked={checked} onChange={testOnChange}/>
             </div>
 
