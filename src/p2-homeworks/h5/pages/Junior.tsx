c2726cb6f5a4adf8ac9a6b91e160adb2d7e8e0f3
Irina Novakovich <irinaredhood@gmail.com>
2022-05-30 15:07:14 +0300

    finished handlers for SuperEditableSpan.tsx

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import HW1 from '../../h1/HW1'
-import HW2 from '../../h2/HW2'
-import HW3 from '../../h3/HW3'
-import HW4 from '../../h4/HW4'
-import Error404 from "./Error404";
+import HW6 from "../../h6/HW6";
 
 function Junior() {
     return (
         <div>
+            <HW6/>
             {/*<HW7/>*/}
             {/*<HW8/>*/}
             {/*<HW9/>*/}
             {/*<HW10/>*/}
             {/*<HW11/>*/}
             {/*<HW12/>*/}
-            <Error404/>
 
         </div>
     )
