64d7dfb63cf1f73559e246348e6ac9b9cc5bc16f
Irina Novakovich <irinaredhood@gmail.com>
2023-02-15 17:10:07 +0300

    HomeWork 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,6 +12,7 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+    //const active = active ? s.active : ''
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +32,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        // style={({isActive}) => ({color: isActive ? '#ad1010' : ''})}
+                         className={({isActive}) => (isActive ? s.active : '')} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,6 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
+                        className={({isActive}) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior
@@ -47,6 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
+                        className={({isActive}) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior Plus
