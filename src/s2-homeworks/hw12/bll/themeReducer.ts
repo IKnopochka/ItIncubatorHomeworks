ee5cdc9fd47261fa75f99a3215e59184a30bd67f
Irina Novakovich <irinaredhood@gmail.com>
2023-02-16 20:03:49 +0300

    HomeWork 12 fixed reducer action type

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,7 +2,7 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state: typeof initState = initState, action: ReturnType<typeof changeThemeId>): typeof initState => { // fix any
+export const themeReducer = (state: typeof initState = initState, action: themeReducerActionTypes): typeof initState => { // fix any
     switch (action.type) {
         // дописать
         case 'SET_THEME_ID' : {
@@ -13,4 +13,6 @@ export const themeReducer = (state: typeof initState = initState, action: Return
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
+
+type themeReducerActionTypes = ReturnType<typeof changeThemeId>
