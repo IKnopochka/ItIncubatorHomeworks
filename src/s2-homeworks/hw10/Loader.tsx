fad09fc58fe7dc4e354e16322d6ab0d7001d1d57
Irina Novakovich <irinaredhood@gmail.com>
2023-02-16 18:56:51 +0300

    HomeWork 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
 import s from './Loader.module.css'
+import loaderImg from './img/Spinner-1s-200px.svg'
 
-export const Loader = () => <div className={s.loader}/>
+
+export const Loader = () => <div className={s.loader} ><img src={loaderImg}/></div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,13 @@
 const initState = {
-    isLoading: false,
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: typeof initState= initState, action: LoadingActionType): typeof initState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
